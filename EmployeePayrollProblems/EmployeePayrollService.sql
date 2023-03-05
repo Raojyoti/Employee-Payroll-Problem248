43199584a732667918a83a0e7de306fae04de27e
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-05 14:23:17 +0530

    [Jyoti Rao] create a employee payroll table

--- EmployeePayrollProblems/EmployeePayrollService.sql
+++ EmployeePayrollProblems/EmployeePayrollService.sql
@@ -1,3 +1,7 @@
 --UC1 Ability to create a payroll service database
 create database payroll_service;
 use payroll_service;
+
+--UC2 Ability to create a employee payroll table in the payroll service database 
+create table  employee_payroll(Id int Identity(1,1)Primary Key, Name varchar(100), Salary float, StartDate date);
+
